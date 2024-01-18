023f40ca371920579e6d4b846bad42e611219129
Shakti Ranjan Mohanta <153294330+shakti57@users.noreply.github.com>
2024-01-18 08:28:25 +0530

    Add files via upload

---
+++ Day21.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+add = lambda x, y: x + y
+result = add(3, 5)
+print(result)  
+
+
+# In[3]:
+
+
+square = lambda x: x**2
+result = square(4)
+print(result)  
+
+
+# In[4]:
+
+
+is_even = lambda x: x % 2 == 0
+result = is_even(7)
+print(result)  
+
+
+# In[5]:
+
+
+concatenate = lambda a, b: a + b
+result = concatenate("Hello", " Shakti")
+print(result)  
+
+
+# In[6]:
+
+
+max_of_three = lambda x, y, z: max(x, y, z)
+result = max_of_three(10, 5, 8)
+print(result)  
+
+
+# In[ ]:
+
+
+
+
