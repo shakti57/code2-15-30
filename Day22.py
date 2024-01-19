24ba2b1943d7d145936a7d3804ea2a940ac205f5
Shakti Ranjan Mohanta <153294330+shakti57@users.noreply.github.com>
2024-01-19 10:22:01 +0530

    Add files via upload

---
+++ Day22.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = np.random.rand(10, 12)
+sns.heatmap(data, cmap="YlGnBu")
+plt.show()
+
+
+# In[7]:
+
+
+import plotly.express as px
+import numpy as np
+
+# Generate random data
+data = np.random.rand(10, 12)
+
+# Create heatmap using Plotly Express
+fig = px.imshow(data, color_continuous_scale="YlGnBu")
+fig.show()
+
+
+# In[ ]:
+
+
+
+
