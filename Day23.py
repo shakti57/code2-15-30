2e30d642507911df20b32296c7b7597655b96c87
Shakti Ranjan Mohanta <153294330+shakti57@users.noreply.github.com>
2024-01-21 08:32:51 +0530

    Add files via upload

---
+++ Day23.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+while True:
+    # Get user input
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    # Perform calculation based on the operator
+    if operator == '+':
+        result = add(num1, num2)
+    elif operator == '-':
+        result = subtract(num1, num2)
+    elif operator == '*':
+        result = multiply(num1, num2)
+    elif operator == '/':
+        result = divide(num1, num2)
+    else:
+        print("Invalid operator")
+        continue
+
+    # Display the result
+    print(f"Result: {result}")
+
+    # Ask if the user wants to perform another calculation
+    another_calculation = input("Do you want to perform another calculation? (yes/no): ").lower()
+    if another_calculation != 'yes':
+        break
+
+
+# In[ ]:
+
+
+
+
