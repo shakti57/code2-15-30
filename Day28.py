403f4027f65e547e9243e2bac297fd72b7024c6a
Shakti Ranjan Mohanta <153294330+shakti57@users.noreply.github.com>
2024-01-29 18:13:54 +0530

    Add files via upload

---
+++ Day28.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
+
+# Generate some random data for demonstration
+np.random.seed(42)
+X = np.sort(5 * np.random.rand(80, 1), axis=0)
+y = np.sin(X).ravel() + np.random.normal(0, 0.1, X.shape[0])
+
+# Create a Decision Tree Regressor
+regressor = DecisionTreeRegressor(max_depth=5)
+
+# Fit the regressor to the data
+regressor.fit(X, y)
+
+# Predict on new data
+X_test = np.arange(0.0, 5.0, 0.01)[:, np.newaxis]
+y_pred = regressor.predict(X_test)
+
+# Plot the results
+plt.figure()
+plt.scatter(X, y, s=20, edgecolor="black", c="darkorange", label="data")
+plt.plot(X_test, y_pred, color="cornflowerblue", label="prediction")
+plt.xlabel("data")
+plt.ylabel("target")
+plt.title("Decision Tree Regression")
+plt.legend()
+plt.show()
+
+
+# In[ ]:
+
+
+
+
